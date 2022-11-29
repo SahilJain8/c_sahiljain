e1f830b2ad5d9565b9d3b01f8bcde416d90ec1f8
Sahil <68115080+SahilJain8@users.noreply.github.com>
2022-11-29 17:04:50 +0530

    Create DNF.cpp

---
+++ DNF.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+
+void sort012(int *arr, int n)
+{        
+    
+    int l=0,m=0,h=n-1;
+    while(m<=h){
+        if(arr[m]==0){
+            swap(arr[l],arr[m]);
+            l++;
+            m++;
+        }
+       else if(arr[m]==1){
+            m++;
+        }
+       else if(arr[m]==2){
+            swap(arr[h],arr[m]);
+            h--;
+        }
+    }
+    
+    
