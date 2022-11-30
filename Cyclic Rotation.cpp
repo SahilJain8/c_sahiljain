bff394f5e8292cf8497832f9d7615d3b51d1cc03
Sahil <68115080+SahilJain8@users.noreply.github.com>
2022-11-30 09:06:36 +0530

    Create Cyclic Rotation.cpp

---
+++ Cyclic Rotation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    //Write your code here
+    int a;
+    int n;
+    cin>>n;
+    vector<int> arr;
+    for(int i=0;i<n;i++){
+        cin>>arr.push_back(i)
+    }
+    
+    for(int i=k;i<n;i++){
+        cout<<arr[i];
+    }
+    
+    for(int i=0;i<k;i++){
+        cout<<arr[i];
+    }
+    
+
+    return 0;
+}
