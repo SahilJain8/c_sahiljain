b9ed73473acd09e4d6b43ca26e718a0728835dc4
Sahil <68115080+SahilJain8@users.noreply.github.com>
2022-12-07 17:55:52 +0530

    Create 2nd largest elemt.cpp

---
+++ 2nd largest elemt.cpp
@@ -0,0 +1,29 @@
+int largest=arr[0]; 
+
+ int secondlar=-1;
+
+ 
+
+    for(int i=1;i<n;i++)
+
+    {
+
+        if(arr[i]>largest)
+
+        {
+
+            secondlar=largest;
+
+            largest=arr[i];
+
+        }
+
+ 
+
+        if(arr[i]<largest && arr[i]>secondlar)
+
+        secondlar=arr[i];
+
+    }
+
+    return secondlar;
